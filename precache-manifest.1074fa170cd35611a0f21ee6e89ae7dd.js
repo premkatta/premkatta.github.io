e26162d617f6601e726e33aa318463a66ce5c18c
premkatta <prem@bitlab.academy>
2018-11-13 15:54:06 +0530

    initial git

---
+++ precache-manifest.1074fa170cd35611a0f21ee6e89ae7dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60f0f3a5f55721c0f309",
+    "url": "/static/css/main.ba360330.chunk.css"
+  },
+  {
+    "revision": "632f924793cd787317f7",
+    "url": "/static/js/1.632f9247.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d2cbee8dbe88e5e5a0d71f9b602c051",
+    "url": "/static/media/email2.8d2cbee8.svg"
+  },
+  {
+    "revision": "6c4cd019aba344ec788d670e2148fdbe",
+    "url": "/static/media/phone.6c4cd019.svg"
+  },
+  {
+    "revision": "60f0f3a5f55721c0f309",
+    "url": "/static/js/main.60f0f3a5.chunk.js"
+  },
+  {
+    "revision": "fe09d133f44c8ee650238b0953cb425c",
+    "url": "/static/media/linkedin2.fe09d133.svg"
+  },
+  {
+    "revision": "be47f1a62f5179fd7b55d3e0ceb396ae",
+    "url": "/static/media/github.be47f1a6.svg"
+  },
+  {
+    "revision": "68a1779ce95d11c42f0669a67dc0b394",
+    "url": "/static/media/facebook.68a1779c.svg"
+  },
+  {
+    "revision": "25431ad807ab3a8e5520c99f51623a7d",
+    "url": "/static/media/twitter.25431ad8.svg"
+  },
+  {
+    "revision": "da9ec757f392b4208e82a2fcc40a834f",
+    "url": "/index.html"
+  }
+];
